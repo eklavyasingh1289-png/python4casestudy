044c78a33955ba6795494d0e4edfade10a8343ed
Eklavya SIngh <eklavyasingh1289@gmail.com>
2026-03-26 23:31:31 +0530

    Delete simple_chatbot_implementation directory

--- simple_chatbot_implementation/test_api_key.py
+++
@@ -1,9 +0,0 @@
-from gemini_engine import GeminiClient as clank
-api_key = input("Enter api key\n")
-gemini = clank(api_key)
-command = "Hey there!"
-while(command!="quit"):
-    prompt = f"{command}"
-    ans = gemini.send_msg(prompt)
-    print(f"{ans}")
-    command = input()
